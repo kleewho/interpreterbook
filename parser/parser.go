938b9ae358e3425f3c9fafb0e8508cbb5f2772d1
Lukasz Klich <klich.lukasz@gmail.com>
2024-04-18 21:20:46 +0200

    Parsing left side of let statement

---
+++ parser/parser.go
@@ -0,0 +1,90 @@
+package parser
+
+import (
+	"monkey/ast"
+	"monkey/lexer"
+	"monkey/token"
+)
+
+type Parser struct {
+	l         *lexer.Lexer
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+
+		p.nextToken()
+	}
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	}
+	return nil
+}
+
+func (p *Parser) parseLetStatement() ast.Statement {
+	letStmt := ast.LetStatement{
+		Token: p.curToken,
+	}
+	if !p.peekAndMaybeNext(token.IDENT) {
+		return nil
+	}
+
+	letStmt.Name = &ast.Identifier{
+		Token: p.curToken,
+		Value: p.curToken.Literal,
+	}
+
+	if !p.peekAndMaybeNext(token.ASSIGN) {
+		return nil
+	}
+
+	expression := p.parseExpression()
+
+	letStmt.Value = expression
+
+	return &letStmt
+}
+
+func (p *Parser) peekAndMaybeNext(tokenType token.TokenType) bool {
+	if p.peekToken.Type == tokenType {
+		p.nextToken()
+		return true
+	} else {
+		return false
+	}
+}
+
+func (p *Parser) parseExpression() ast.Expression {
+	for p.curToken.Type != token.SEMICOLON {
+		p.nextToken()
+	}
+	return nil
+}
